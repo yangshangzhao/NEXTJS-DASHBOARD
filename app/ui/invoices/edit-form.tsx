70155e9b7637696b689e43e589d01bf4df4c9d91
yangshangzhao <13823146317@163.com>
2025-12-19 00:24:36 +0800

    Form验证

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions'
 import {
   CheckIcon,
@@ -9,7 +8,8 @@ import {
 } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import { Button } from '@/app/ui/button'
-import { updateInvoice } from '@/app/lib/actions'
+import { State, updateInvoice } from '@/app/lib/actions'
+import { useFormState } from 'react-dom'
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,12 +19,16 @@ export default function EditInvoiceForm({
   customers: CustomerField[]
 }) {
   // Patch the lint error by using a wrapper function for the async updateInvoice call
-  async function handleUpdateInvoice(formData: FormData) {
-    await updateInvoice(invoice.id, formData)
-  }
+  // async function handleUpdateInvoice(formData: FormData) {
+  //   await updateInvoice(invoice.id, formData)
+  // }
+
+  const initialState: State = { message: null, errors: {} }
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState)
 
   return (
-    <form action={handleUpdateInvoice}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
